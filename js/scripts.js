b8079fcaa83f76c822acb8da116e5d1e63a144c6
marynganga <marynganga.mw@gmail.com>
2017-08-18 15:37:00 +0300

    Fulfill Spec 1 - Count up to the provided number from 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+//Back End Logic
+var total = [];
+function countUp(input){
+	
+	for (var index = 1; index <= input; index++) {
+		total += index;
+	};
+	return total;
+	
+};
 
 // Front End Logic
 $(document).ready(function(){
@@ -6,7 +16,11 @@ $(document).ready(function(){
 		//store the user input into a variable
 		var number = parseInt($("#inputNumber").val());
 		//test
-		console.log(number)
+		//console.log(number)
+
+		//Call the function countUp to work on number in a new variable
+		var countedNumbers = countUp(number);
+			console.log(countedNumbers);
+	});
 		
-	})
-})
+});
